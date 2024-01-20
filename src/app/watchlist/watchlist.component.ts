ad4e2117dbb61c5e604618bd53fd35509931f29f
Hagar <hagermoh21@gmail.com>
2024-01-20 08:04:45 +0200

    transfer default page in watchlist to the empty-watchlist component, and then call it there.

--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { EmptyWatchlistComponent } from '../empty-watchlist/empty-watchlist.component';
 
 @Component({
   selector: 'app-watchlist',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, CommonModule],
+  imports: [RouterLink, RouterOutlet, CommonModule, EmptyWatchlistComponent],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.css',
 })
