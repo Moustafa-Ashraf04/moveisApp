fceaac08bce8a885d2b986082e76295e0ba9b425
Hagar <hagermoh21@gmail.com>
2024-01-21 06:46:15 +0200

    implement the movie wishlist card to increase reusability and readability and to apply concept one task at a time.

--- src/app/movie-watchlist-card/movie-watchlist-card.component.ts
+++ src/app/movie-watchlist-card/movie-watchlist-card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from '../interface/movies';
 
 @Component({
   selector: 'app-movie-watchlist-card',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './movie-watchlist-card.component.css'
 })
 export class MovieWatchlistCardComponent {
+  @Input() watchii!: Movie;
 
+  fillColor: string = '#000000';
+
+  changeFillColor(): void {
+    this.fillColor = this.fillColor === '#000000' ? '#ffe353' : '#000000';
+  }
+
+  preventHeartClick(event: Event): void {
+    // Prevent the click event from propagating to the card
+    event.stopPropagation();
+  }
 }
--- src/app/watchlist/watchlist.component.ts
+++ src/app/watchlist/watchlist.component.ts
@@ -4,11 +4,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { EmptyWatchlistComponent } from '../empty-watchlist/empty-watchlist.component';
 import { Movie } from '../interface/movies';
 import { FavoriteServiceService } from '../services/favorite-service.service';
+import { MovieWatchlistCardComponent } from '../movie-watchlist-card/movie-watchlist-card.component';
 
 @Component({
   selector: 'app-watchlist',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, CommonModule, EmptyWatchlistComponent],
+  imports: [RouterLink, RouterOutlet, CommonModule, EmptyWatchlistComponent, MovieWatchlistCardComponent],
   templateUrl: './watchlist.component.html',
   styleUrl: './watchlist.component.css',
 })
@@ -20,17 +21,9 @@ export class WatchlistComponent {
   ngOnInit(){
     // this.watchlistService.getFavorite().subscribe(val => console.log(this.watchlistMovies.push(val)))
     this.watchlistService.getFavorite().subscribe(val => console.log(this.watchlistMovies=val, val, this.watchlistMovies.length))
+    console.log(typeof(this.watchlistMovies[0]));
   }
 
 
-  fillColor: string = '#000000';
-
-  changeFillColor(): void {
-    this.fillColor = this.fillColor === '#000000' ? '#ffe353' : '#000000';
-  }
   
-  preventHeartClick(event: Event): void {
-    // Prevent the click event from propagating to the card
-    event.stopPropagation();
-  }
 }
