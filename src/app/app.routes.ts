054d2ebbdd02f8ad79cc21a001e84494329c7334
Hagar <hagermoh21@gmail.com>
2024-02-20 14:18:35 +0200

    commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   {
-    path: '/',
+    path: 'home',
     component: MoviesHomeListComponent,
     title: 'Home',
   },
